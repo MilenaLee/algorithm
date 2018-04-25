04f204a43f7990afca38c7dabe01a7b5d649d7f5
MilenaLee <leemihyun90@gmail.com>
2018-04-26 00:00:25 +0900

    307. Range Sum Query - Mutable
    https://leetcode.com/problems/range-sum-query-mutable

---
+++ Leetcode/307. Range Sum Query - Mutable.py
@@ -0,0 +1,54 @@
+﻿class NumArray(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.nums = [0] + nums
+        self.fenwick = [0 for _ in range(len(nums) + 1)]
+        
+        for i, num in enumerate(nums):
+            idx = i + 1
+            while idx < len(self.fenwick):
+                self.fenwick[idx] += num
+                idx += (idx & -idx)
+
+                
+    def update(self, i, val):
+        """
+        :type i: int
+        :type val: int
+        :rtype: void
+        """
+        i += 1
+        update_value = val - self.nums[i]
+        self.nums[i] = val
+
+        while i < len(self.fenwick):
+            self.fenwick[i] += update_value
+            i += (i & -i)
+        
+        
+    def _sumRange(self, i):
+        ret = 0
+        while i > 0:
+            ret += self.fenwick[i]
+            i &= (i-1)
+        return ret
+
+    
+    def sumRange(self, i, j):
+        """
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        i, j = i, j+1
+        return self._sumRange(j) - self._sumRange(i)
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# obj.update(i,val)
+# param_2 = obj.sumRange(i,j)
